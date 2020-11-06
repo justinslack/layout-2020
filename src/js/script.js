683b1923193316ebf84e1e4b7369e73ad1c85ecf
Justin Slack <justin@justinslack.com>
2020-11-06 17:06:43 +0200

    Add layout examples and scripts

--- src/js/script.js
+++ src/js/script.js
@@ -1,2 +1,23 @@
 // Add some js
 
+document.addEventListener("DOMContentLoaded", function (event) {
+	var element = document.querySelectorAll(".toggle");
+
+	if (element) {
+		element.forEach(function (el, key) {
+			el.addEventListener("click", function () {
+				console.log(key);
+
+				el.classList.toggle("active");
+
+				element.forEach(function (ell, els) {
+					if (key !== els) {
+						ell.classList.remove("active");
+					}
+					console.log(els);
+				});
+			});
+		});
+	}
+});
+  
